14d6a8c031ff773999c5c8e7fbabb616860611c8
Pedro de Jesus <pedro.jesus@talkdesk.com>
2019-06-26 16:56:48 +0100

    Updated the decorator to support data-store versions >1.0.0
    Created an integration test for the decorator with an in memory store
    that implements the data-store interface.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,26 @@ module.exports = function promptWithPersistence(store, prompt) {
     const originalGetInput = prompt.getInput;
     const originalGet = prompt.get;
 
+    function parsePropPathForDataStore(propPath) {
+        if(!Array.isArray(propPath)) {
+            return propPath;
+        }
+
+        return propPath.join('.');
+    }
+
     prompt.getInput = function (prop, callback) {
+        const propPath = parsePropPathForDataStore(prop.path);
+
         const nextProp = {
             ...prop,
             schema: {
                 ...prop.schema,
-                default: prop.schema.persisted && store.hasOwn(prop.path) ? store.get(prop.path) : prop.schema.default,
+                default: prop.schema.persisted && store.hasOwn(propPath) ? store.get(propPath) : prop.schema.default,
                 before: value => {
                     const nextValue = prop.schema.before ? prop.schema.before(value) : value;
                     if (prop.schema.persisted) {
-                        store.set(prop.path, nextValue);
+                        store.set(propPath, nextValue);
                     }
                     return nextValue;
                 }
